7056b8931aedd34a6987f82ecb65fc4d50e848a8
Эсонов Жахонгир <Zhakhongir.Esonov@softline.com>
2023-03-12 16:07:22 +0300

    ProductRepositoryTest.kt and build.gradle update

--- src/test/kotlin/com/example/spring_data_jpa_lessons/repository/ProductRepositoryTest.kt
+++ src/test/kotlin/com/example/spring_data_jpa_lessons/repository/ProductRepositoryTest.kt
@@ -1,13 +1,12 @@
 package com.example.spring_data_jpa_lessons.repository
 
-import com.example.spring_data_jpa_lessons.SpringDataJpaLessonsApplication
 import com.example.spring_data_jpa_lessons.entity.Product
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import java.math.BigDecimal
 
-@SpringBootTest(classes = [SpringDataJpaLessonsApplication::class])
+@SpringBootTest
 class ProductRepositoryTest(
     @Autowired
     private val productRepository: ProductRepository
@@ -16,7 +15,7 @@ class ProductRepositoryTest(
     @Test
     internal fun saveMethod() {
         val productTwo = Product()
-        productTwo.sku = "100ABC"
+        productTwo.sku = "300ABC"
         productTwo.name = "product 1"
         productTwo.description = "product 1 description"
         productTwo.price = BigDecimal(100)
@@ -25,6 +24,15 @@ class ProductRepositoryTest(
         val saveObject = productRepository.save(productTwo)
         println(saveObject.id)
         println(productTwo)
+    }
+
+    @Test
+    internal fun updateUsingSaveMethod() {
+        val id = 1L
+        val product = productRepository.findById(id).get()
+        product.name = "updated product 1"
+        product.description = "updated product 1 desc"
+        productRepository.save(product)
     }
 
 }
