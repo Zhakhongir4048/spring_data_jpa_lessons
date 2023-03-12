016bedf72e7977a3786de3dff4196acd8b195049
Эсонов Жахонгир <Zhakhongir.Esonov@softline.com>
2023-03-12 17:14:11 +0300

    ProductRepositoryTest.kt update

--- src/test/kotlin/com/example/spring_data_jpa_lessons/repository/ProductRepositoryTest.kt
+++ src/test/kotlin/com/example/spring_data_jpa_lessons/repository/ProductRepositoryTest.kt
@@ -14,16 +14,16 @@ class ProductRepositoryTest(
 
     @Test
     internal fun saveMethod() {
-        val productTwo = Product()
-        productTwo.sku = "300ABC"
-        productTwo.name = "product 1"
-        productTwo.description = "product 1 description"
-        productTwo.price = BigDecimal(100)
-        productTwo.imageUrl = "product 1 imageUrl"
-        productTwo.active = true
-        val saveObject = productRepository.save(productTwo)
+        val productOne = Product()
+        productOne.sku = "100ABC"
+        productOne.name = "product 1"
+        productOne.description = "product 1 description"
+        productOne.price = BigDecimal(100)
+        productOne.imageUrl = "product 1 imageUrl"
+        productOne.active = true
+        val saveObject = productRepository.save(productOne)
         println(saveObject.id)
-        println(productTwo)
+        println(productOne)
     }
 
     @Test
@@ -33,6 +33,35 @@ class ProductRepositoryTest(
         product.name = "updated product 1"
         product.description = "updated product 1 desc"
         productRepository.save(product)
+    }
+
+    @Test
+    internal fun findByIdMethod() {
+        val id = 1L
+        val product = productRepository.findById(id).get()
+        println(product)
+    }
+
+    @Test
+    internal fun saveAllMethod() {
+        val productFour = Product()
+        productFour.sku = "400ABC"
+        productFour.name = "product 4"
+        productFour.description = "product 4 description"
+        productFour.price = BigDecimal(400)
+        productFour.imageUrl = "product 4 imageUrl"
+        productFour.active = true
+
+        val productFive = Product()
+        productFive.sku = "500ABC"
+        productFive.name = "product 5"
+        productFive.description = "product 5 description"
+        productFive.price = BigDecimal(500)
+        productFive.imageUrl = "product 5 imageUrl"
+        productFive.active = true
+
+        productRepository.saveAll(arrayListOf(productFour, productFive))
+
     }
 
 }
